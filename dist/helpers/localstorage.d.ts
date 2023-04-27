5d6f4174413a2fb4033b0b9ce59ce4a1c61d9553
Arvid Hast <arvid@mealme.ai>
2023-04-27 02:19:21 +0200

    fix: update tsc

--- dist/helpers/localstorage.d.ts
+++ dist/helpers/localstorage.d.ts
@@ -1,4 +1,4 @@
-declare const saveExperimentData: (name: any, data: any) => void;
-declare const getSavedExperimentData: (name: any) => any;
-export { getSavedExperimentData, saveExperimentData };
+export function getSavedExperimentData(name: any): any;
+export function saveExperimentData(name: any, data: any): void;
+export function preventDuplicateRequest(object: any, callback: any): void;
 //# sourceMappingURL=localstorage.d.ts.map
